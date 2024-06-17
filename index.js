e2f22b6c9b86d3edfd08c7a62c185d829c426861
okifxon <shokirjonovokifxon46@gmail.com>
2024-06-17 17:43:47 +0500

    dars

---
+++ index.js
@@ -0,0 +1,170 @@
+// IIfi 1masala
+
+// (function(){
+//     console.log('hello');
+// })()    
+
+// bu iifini oddiy korinishi
+
+// ((a,b) =>{
+//     return a * b;
+// })(2,5);
+// bu arrow functionda yozilgan xolati
+
+// (function (a,b){
+//     return a / b;
+// })(6,2)
+// bu esa function decloration xolati
+
+
+// 1masala
+// let massiv = [2, 4, 5, 7,  6, 8, 10];
+
+// function massjuft(arg) {
+//     let res = arg.filter(function(value){
+//         return value % 2 == 0
+//     })
+//     return res
+// };
+// console.log(massjuft(massiv));
+
+
+// 2masala
+
+
+
+// 3masala
+// let a = 5
+
+// function func(arg) {
+//     return arg ** 5
+// }
+// console.log(func(a));
+
+// pure function bu ozgarmas funksiya degan manoda kelad maslan unga tashqi tasir bolmaydi pure function har doim bir xil natija qaytaradi
+
+
+// 4masala 
+
+    
+// const call = (callback) =>{
+//     let number = 50;
+//     callback(number)
+// }
+
+// call(function(value){
+//     console.log(value);
+// })
+
+// 4 berilgan sondan boshlab 1 gacha teskari sanovchi funksiya
+
+
+
+
+
+
+// massivlarga oid masalalar
+
+// 1
+
+// const son = [1,2,3,4,5,6,7];
+
+
+// const mayfunk = function(arrow){
+//     return arrow.slice(-5, 5);
+// }
+
+// console.log(mayfunk(son));
+
+// 3
+
+
+// const massiv = [1, 2, 3];
+// const mass = [4, 5, 6];
+// const masss = [7, 8, 9];
+
+// const func = function (massivent, masss,  mains){
+//     return massivent.concat(masss) + ',' + mains
+// }
+
+// console.log(func(massiv,mass,masss));
+
+// 4
+
+// 5
+
+// 7 
+
+// 8
+
+// 9
+
+// const ret = [4,,8,5,3];
+
+// const funck = function (arrow){
+//     const geret = arrow.map(function (arr){
+//         result = arr.value % 2 == 0 ** 2
+//         return result
+//     })
+    
+// }
+
+// console.log(funck(ret));
+
+
+
+// objectlar  
+
+// 1masala
+// let objectkey = {
+//     name:'javlon',
+//     age:30
+// };
+
+
+
+// const objk = function (obj){
+//     const res = Object.keys(objectkey);
+//     return res
+// }
+// console.log(objk(objectkey));
+
+
+// 2masala
+// let objectkey = {
+//     name:'javlon',
+//     age:30
+// };
+
+
+
+// const objk = function (obj){
+//     const res = Object.values(objectkey);
+//     return res
+// }
+// console.log(objk(objectkey));
+
+// 3masala
+// let objectkey = {
+//     name:'javlon',
+//     age:30
+// };
+
+
+
+// const objk = function (obj){
+    
+//         const res = Object.entries(objectkey);
+//         return res
+    
+// }
+// console.log(objk(objectkey));
+
+
+
+// 4masala
+// savolga tushunmadim va ishlay olmadim
+
+
+
+
